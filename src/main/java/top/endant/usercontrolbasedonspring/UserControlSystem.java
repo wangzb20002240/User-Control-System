16293b47d16b0023b4b3d1053e115321fbf9e13f
endant <752239019@qq.com>
2023-05-31 14:35:57 +0800

    1.0

---
+++ src/main/java/top/endant/usercontrolbasedonspring/UserControlSystem.java
@@ -0,0 +1,13 @@
+package top.endant.usercontrolbasedonspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserControlSystem {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserControlSystem.class, args);
+    }
+
+}
